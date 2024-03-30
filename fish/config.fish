3c594ce81972c7c0f8f70196f5c12aae98b63a2e
Martin Lévesque <levesque.martin@gmail.com>
2024-03-30 10:57:36 -0400

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+#> fisher list
+#jorgebucaran/fisher
+#jethrokuan/fzf
+
 # aliases
 alias vi 'nvim'
 alias vim 'nvim'
